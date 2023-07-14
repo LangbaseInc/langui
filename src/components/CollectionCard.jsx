1846e14e6359d9f62e6cf5d4574822b13b0b1970
Ahmad Bilal <ahmadbilaldev@gmail.com>
2023-07-14 07:30:24 +0500

    👌 IMPROVE: Replace emojis with svg icons

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,24 +1,50 @@
 import Link from 'next/link'
+import * as Icons from '@component/CollectionIcons'
 
 export default function CollectionCard({ componentData }) {
   const componentCountPluralize =
     componentData.count > 1 ? 'Components' : 'Component'
   const componentCount = `${componentData.count} ${componentCountPluralize}`
 
+  const IconData = {
+    Alerts: Icons.Alerts,
+    Buttons: Icons.Buttons,
+    Cards: Icons.Cards,
+    Badges: Icons.Badges,
+    ChatContainers: Icons.ChatContainers,
+    ChatInputs: Icons.ChatInputs,
+    ChatMessages: Icons.ChatMessages,
+    Inputs: Icons.Inputs,
+    Pricing: Icons.Pricing,
+    Toggles: Icons.Toggles,
+    Sidebars: Icons.Sidebars,
+    CardsforPrompts: Icons.PromptCards,
+  }
+  const Icon = IconData[componentData.title.replace(/\s/g, '')]
+
   return (
     <Link href={`/${componentData.category}/${componentData.slug}`}>
-      <div className="group relative block h-full">
-        <div className="rounded-lg bg-slate-800 transition group-hover:-translate-y-2 ltr:group-hover:-translate-x-2 rtl:group-hover:translate-x-2">
-          <div className="p-4 sm:p-6">
-            <span aria-hidden="true" role="img" className="text-lg sm:text-xl">
-              {componentData.emoji}
+      <div className="group relative block h-full backdrop-blur-sm">
+        <div className="rounded-lg border-2 border-transparent bg-slate-800/20 transition-colors group-hover:border-blue-600">
+          <div className="flex  justify-between p-4 sm:p-6">
+            <div className="flex flex-col">
+              <h2 className="font-medium text-slate-200 sm:text-lg">
+                {componentData.title}
+              </h2>
+              {/* <p className="mt-4 text-sm leading-6 text-slate-400">
+                {componentData.description}
+              </p> */}
+              <p className="font-meium mt-2 text-sm text-slate-500">
+                {componentCount}
+              </p>
+            </div>
+            <span
+              aria-hidden="true"
+              role="img"
+              className="mx-2 text-lg text-slate-200 sm:text-2xl"
+            >
+              {Icon}
             </span>
-
-            <h2 className="mt-4 font-medium text-slate-200 sm:text-lg">
-              {componentData.title}
-            </h2>
-
-            <p className="mt-2 text-xs text-slate-300">{componentCount}</p>
           </div>
         </div>
       </div>
---
+++ src/components/CollectionIcons.jsx
@@ -0,0 +1,199 @@
+export const Alerts = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4"></path>
+    <path d="M12 11l0 .01"></path>
+    <path d="M8 11l0 .01"></path>
+    <path d="M16 11l0 .01"></path>
+  </svg>
+)
+
+export const Buttons = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M10 14l11 -11"></path>
+    <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"></path>
+  </svg>
+)
+
+export const Cards = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M3.604 7.197l7.138 -3.109a.96 .96 0 0 1 1.27 .527l4.924 11.902a1 1 0 0 1 -.514 1.304l-7.137 3.109a.96 .96 0 0 1 -1.271 -.527l-4.924 -11.903a1 1 0 0 1 .514 -1.304z"></path>
+    <path d="M15 4h1a1 1 0 0 1 1 1v3.5"></path>
+    <path d="M20 6c.264 .112 .52 .217 .768 .315a1 1 0 0 1 .53 1.311l-2.298 5.374"></path>
+  </svg>
+)
+
+export const Badges = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+  </svg>
+)
+export const ChatContainers = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5"></path>
+    <path d="M12 12l8 -4.5"></path>
+    <path d="M12 12l0 9"></path>
+    <path d="M12 12l-8 -4.5"></path>
+    <path d="M16 5.25l-8 4.5"></path>
+  </svg>
+)
+export const Inputs = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M14 6l7 7l-4 4"></path>
+    <path d="M5.828 18.172a2.828 2.828 0 0 0 4 0l10.586 -10.586a2 2 0 0 0 0 -2.829l-1.171 -1.171a2 2 0 0 0 -2.829 0l-10.586 10.586a2.828 2.828 0 0 0 0 4z"></path>
+    <path d="M4 20l1.768 -1.768"></path>
+  </svg>
+)
+
+export const ChatInputs = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M8 9h8"></path>
+    <path d="M8 13h6"></path>
+    <path d="M12.01 18.594l-4.01 2.406v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5"></path>
+    <path d="M16 19h6"></path>
+    <path d="M19 16v6"></path>
+  </svg>
+)
+
+export const ChatMessages = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
+    <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
+  </svg>
+)
+
+export const Pricing = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"></path>
+    <path d="M12 3v3m0 12v3"></path>
+  </svg>
+)
+
+export const PromptCards = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M8 9l3 3l-3 3"></path>
+    <path d="M13 15l3 0"></path>
+    <path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+  </svg>
+)
+
+export const Toggles = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M16 12m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+    <path d="M2 6m0 6a6 6 0 0 1 6 -6h8a6 6 0 0 1 6 6v0a6 6 0 0 1 -6 6h-8a6 6 0 0 1 -6 -6z"></path>
+  </svg>
+)
+
+export const Sidebars = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+    <path d="M9 4l0 16"></path>
+  </svg>
+)
